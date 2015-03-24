a6af1ad552b034aca701a1cbe34759084651fe66
italo <italobento@hotmail.com>
2015-03-24 14:34:09 -0300

    ...

--- Fruit Catch/AppDelegate.m
+++ Fruit Catch/AppDelegate.m
@@ -281,8 +281,8 @@
     // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
     // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
     
-    [self loadFromWebService];
-    [AppDelegate sendFiletoWebService];
+//    [self loadFromWebService];
+//    [AppDelegate sendFiletoWebService];
 }
 
 - (void)applicationWillEnterForeground:(UIApplication *)application {
